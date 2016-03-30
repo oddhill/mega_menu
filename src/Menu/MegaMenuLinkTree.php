adf6f36652e65bbd4c518cd9f9d442e295abf28b
Christoffer Palm <christoffer@Christoffers-MacBook-Pro-3.local>
2016-03-30 11:04:48 +0200

    Changed how theme suggestions are handled

--- src/Menu/MegaMenuLinkTree.php
+++ src/Menu/MegaMenuLinkTree.php
@@ -12,14 +12,7 @@ class MegaMenuLinkTree extends MenuLinkTree {
   public function build(array $tree) {
     $build = parent::build($tree);
 
-    /** @var \Drupal\Core\Menu\MenuLinkInterface $first_link */
-    $first_link = reset($tree)->link;
-    $menu_name = $first_link->getMenuName();
-
-    // Add a more specific theme suggestion to differentiate this rendered
-    // menu from others.
-    $build['#menu_name'] = $menu_name;
-    $build['#theme'] = 'menu__mega_menu__' . strtr($menu_name, '-', '_');
+    $build['#theme'] = 'menu__mega_menu';
 
     return $build;
   }
